954bcd63c5c4cf2f103a49b3a043b3a94f0d0266
unknown <startup.jerome@gmail.com>
2022-08-23 09:29:57 +0530

    studied about state with useState

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("Programming");
+  const [person, setPerson] = useState({ name: "tony", age: 30 });
+
+  const clickMe = () => {
+    setName("Knowledge")
+    setPerson({ name: "Brijen", age: 24 })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Jerome there!</Text>
-      <StatusBar style="auto" />
+      <Text>{name}</Text>
+      <Text>My name is {person.name} and my age is {person.age}</Text>
+      <View style={styles.button}>
+        <Button title="change"  onPress={clickMe}/>
+      </View>
     </View>
   );
 }
@@ -17,4 +28,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: "red",
+    padding: 10,
+    margin: 20
+  }
 });
