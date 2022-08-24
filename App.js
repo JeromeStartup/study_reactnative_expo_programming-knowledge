246b39884d2ade178738de7f5148e9dc83d5ed1c
unknown <startup.jerome@gmail.com>
2022-08-24 08:38:04 +0530

    image change style changed

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, View } from 'react-native';
+import styles from './styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Jerome there!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.image}>
+        <Image source={require('./assets/marvel.jpg')} />
+
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
