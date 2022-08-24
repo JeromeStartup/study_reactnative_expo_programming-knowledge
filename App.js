d646433352014e7e3f0af2137edd1feccbb6dab0
unknown <startup.jerome@gmail.com>
2022-08-24 08:24:42 +0530

    studied about Alert

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, Text, TextInput, View } from 'react-native';
+import styles from './styles';
 
 export default function App() {
+  const [age, setAge] = useState(0);
+
+  const ageHandler = () => {
+    if (age > 21) {
+      Alert.alert("Yay!!", "You can drink")
+    } else {
+      Alert.alert("oops", "Sorry, You can not drink")
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Jerome there!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input} />
+      <TextInput style={styles.input} onChangeText={(text) => setAge(text)} />
+      <Button title="Can I Drink" onPress={() => ageHandler()} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: 40,
+        padding: 20
+    },
+    input: {
+        backgroundColor: "yellow",
+        padding: 20,
+        borderWidth: 2,
+        marginTop: 10
+    }
+});
+
+export default styles;
