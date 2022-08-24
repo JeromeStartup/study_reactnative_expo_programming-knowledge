f2017f7b2cb53155ba364217d0115cb790f845e0
unknown <startup.jerome@gmail.com>
2022-08-24 07:20:15 +0530

    studied about list & scrollview

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  const [fruit, setFruit] = useState([
+    { name: "Apple", key: "1" },
+    { name: "Mango", key: "2" },
+    { name: "Banana", key: "3" },
+    { name: "Orange", key: "4" },
+    { name: "grapes", key: "5" },
+    { name: "starawberry", key: "6" },
+    { name: "pineapple", key: "7" },
+    { name: "water melon", key: "8" },
+    { name: "papaw", key: "9" }
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Jerome there!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        {
+          fruit.map((item) => {
+            return (
+              <View key={item.key}>
+                <Text style={styles.item}>{item.name}</Text>
+              </View>
+            )
+          })
+        }
+      </ScrollView>
     </View>
   );
 }
@@ -14,7 +34,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  item: {
+    fontSize: 22,
+    backgroundColor: "yellow",
+    marginTop: 20,
+    padding: 20
+  }
 });
