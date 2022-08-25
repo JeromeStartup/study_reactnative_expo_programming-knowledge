3004da6c77fc406f74ffc7c6b14cd8b878373030
unknown <startup.jerome@gmail.com>
2022-08-26 00:43:41 +0530

    badges in react-native

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { View } from 'react-native';
+import { Avatar, Badge } from 'react-native-elements';
 import styles from './styles';
 
 export default function App() {
+  const [message, setMessage] = useState(10)
   return (
     <View style={styles.container}>
-   
+      <View>
+        <Avatar
+          rounded
+          source={{
+            uri: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+          }}
+          size="large"
+        />
+      </View>
+      <Badge
+        value={message}
+        containerStyle={{ position: "absolute", left: 80, top: 25 }}
+      />
     </View>
   );
 }
