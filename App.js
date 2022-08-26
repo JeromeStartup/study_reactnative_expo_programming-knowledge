f837636f4bae9fcf7dbe756c9f7ea0814ac2ad12
unknown <startup.jerome@gmail.com>
2022-08-26 13:14:06 +0530

    react native elements button

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { View } from 'react-native';
+import { Button } from 'react-native-elements';
 import styles from './styles';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-   
+      <Button
+        title="Solid Button"
+        type="solid"
+        titleStyle={{ color: "yellow", fontSize: 30 }}
+      />
+      <Button
+        title="Outline Button"
+        type="outline"
+        containerStyle={{ backgroundColor: "yellow" }}
+        titleStyle={{ color: "red", fontSize: 30 }}
+      />
+      <Button
+        title="Raise Button"
+        type="solid"
+        raised
+      />
+      <Button
+        icon={<Ionicons name="md-checkmark-circle" size={32} color="green" />}
+        title="Icon Button"
+        color="white"
+      />
+
+      <Button title="loading" loading />
+      <Button title="loading" loading={false} />
     </View>
   );
 }
