72a5a864f7cbbc5d00bb67c9d2464875f29c03fc
unknown <startup.jerome@gmail.com>
2022-09-04 14:32:27 +0530

    done studing about checkboxes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Alert, View } from 'react-native';
+import { Alert, Button, View } from 'react-native';
 import { CheckBox } from 'react-native-elements';
 import styles from './styles';
 
@@ -15,25 +15,27 @@ export default function App() {
       hobbies.push("reading")
     }
     if (writing === true) {
-      hobbies.push("reading")
+      hobbies.push("writing")
     }
     if (music === true) {
-      hobbies.push("reading")
+      hobbies.push("music")
     }
 
     Alert.alert("Hobbies", "Hello user, your hobbies are " + hobbies.toString())
   }
   return (
     <View style={styles.container}>
-      <CheckBox 
-      title="Reading" 
-      checked={reading} 
-      onPress={() => setReading(!reading)}
-      checkedIcon="dot-circle-o"
-      uncheckedIcon="circle-o"
+      <CheckBox
+        title="Reading"
+        checked={reading}
+        onPress={() => setReading(!reading)}
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
       />
-      <CheckBox title="Writing" checked={writing} onPress={() => setReading(!writing)} />
-      <CheckBox title="Music" checked={music} onPress={() => setReading(!music)} />
+      <CheckBox title="Writing" checked={writing} onPress={() => setWriting(!writing)} checkedTitle="Writing is my hobby" />
+      <CheckBox title="Music" checked={music} onPress={() => setMusic(!music)} checkedColor="red" />
+
+      <Button title='Click' onPress={click} />
     </View>
   );
 }
