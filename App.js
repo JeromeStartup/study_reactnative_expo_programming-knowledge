72336a5379af5e2e2d2dd7707c86687461ae72f9
unknown <startup.jerome@gmail.com>
2022-09-04 14:16:23 +0530

    done studing button

--- App.js
+++ App.js
@@ -5,21 +5,57 @@ import styles from './styles';
 
 export default function App() {
   const [name, setName] = useState("");
-  
+
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder='enter your name'
-        value={name}
-        onChangeText={(text) = setName(text)}
-      />
 
-      <Button
-        title="Submit"
-        type="solid"
-        containerStyle={{ marginTop: 20 }}
-        onPress={() => Alert.alert("Welcome", "Hello" + name + ", welcome to our app")}
-      />
+      <View style={styles.container}>
+        <Button
+          title="Solid Button"
+          type="solid"
+          titleStyle={{ color: "yellow", fontSize: 30 }}
+
+        />
+
+        <Button
+          title="Outline Button"
+          type="outline"
+          titleStyle={{ color: "red" }}
+          containerStyle={{ marginTop: 10, backgroundColor: "yellow" }}
+        />
+
+        <Button
+          title="Solid Button"
+          type="solid"
+          titleStyle={{ color: "yellow", fontSize: 30 }}
+          containerStyle={{ marginTop: 10 }}
+          raised
+        />
+
+        <Button
+          title="loading"
+          type="solid"
+          titleStyle={{ color: "yellow", fontSize: 30 }}
+          containerStyle={{ marginTop: 10 }}
+          loading={true}
+        />
+      </View>
+
+      <View>
+        <TextInput
+          placeholder='enter your name'
+          value={name}
+          onChangeText={(text) => setName(text)}
+          onSubmitEditing={() => Alert.alert("Welcome", "Hello" + name + ", welcome to our app")}
+        />
+
+        <Button
+          title="Submit"
+          type="solid"
+          containerStyle={{ marginTop: 20 }}
+          onPress={() => Alert.alert("Welcome", "Hello" + name + ", welcome to our app")}
+        />
+      </View>
     </View>
   );
 }
